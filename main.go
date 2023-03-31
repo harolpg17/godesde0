288970d68599306d974160021cfa717c8e8b2945
Harol Perez Giraldo <harolpg17@hotmail.com>
2023-03-31 17:25:06 -0500

    Trabajando con variables

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/harolpg17/godesde0/variables"
 )
 
 func main() {
 	variables.MuestroEnteros()
+	variables.RestoVariables()
+
+	estado, texto := variables.ConviertoaTexto(145)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
